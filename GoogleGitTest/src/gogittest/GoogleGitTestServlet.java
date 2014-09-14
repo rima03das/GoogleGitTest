351812b85f19b2995336f7a0d761c8840a52fc47
Project X <Project X@abc-PC>
2014-09-14 13:39:42 +0530

    First Commit

---
+++ GoogleGitTest/src/gogittest/GoogleGitTestServlet.java
@@ -0,0 +1,13 @@
+package gogittest;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GoogleGitTestServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException { 
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
